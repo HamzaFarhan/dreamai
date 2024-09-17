55e6b39e759bc7073eb7e2f572ab84c3d2eb1ba8
Hamza Farhan <thehamza96@gmail.com>
2024-09-17 16:46:42 +0500

    latest

--- ui.py
+++ ui.py
@@ -6,18 +6,19 @@ import pandas as pd
 from fasthtml.common import (
     H1,
     H3,
+    H5,
     H6,
-    Container,
     Div,
     Form,
     Img,
     Input,
     Li,
+    Link,
+    Main,
     Ol,
     Option,
     P,
     Select,
-    Style,
     Ul,
     fast_app,
     serve,
@@ -26,26 +27,13 @@ from loguru import logger
 
 from dreamai.md_utils import docs_to_md
 
-# LANCE_DIR = Path("/home/hamza/dev/lance")
-LANCE_DIR = Path("/home/hamza/dev/empty_lance")
+LANCE_DIR = Path("/home/hamza/dev/lance")
+# LANCE_DIR = Path("/home/hamza/dev/empty_lance")
 
-TABLES_LIST_LIMIT = 5
+headers = (Link(rel="stylesheet", href="style_ui.css"),)
 
-style = """
-.ui-indicator{
-    display: none;
-}
-.htmx-request .ui-indicator{
-    display: inline;
-    color: #007bff;  /* Blue color */
-}
-.htmx-request.ui-indicator{
-    display: inline;
-    color: #007bff;  /* Blue color */
-}
-"""
 
-app = fast_app(live=True, hdrs=(Style(style),))[0]
+app = fast_app(live=True, hdrs=headers)[0]
 
 
 def get_sorted_indexes(index_folder: str | Path = LANCE_DIR) -> list[str]:
@@ -65,99 +53,109 @@ def divider():
     )
 
 
-def document_input(hx_swap_oob: str = "false"):
-    return Input(
-        type="file",
-        name="documents",
-        multiple=True,
-        accept=".pdf,.docx,.txt,.md",
-        id="document-input",
-        hx_swap_oob=hx_swap_oob,
-    )
+def documents_upload(current_index: str, swap_oob: str | None = None):
+    components = [
+        H3("Upload Documents (PDF, DOCX, TXT, MD)"),
+    ]
+    if not current_index:
+        components.append(P("No index selected. Please select an index to upload documents."))
+    else:
+        components += [
+            H5(f"Index: {current_index}") if current_index else None,
+            Form(
+                Input(
+                    type="file",
+                    name="documents",
+                    multiple=True,
+                    accept=".pdf,.docx,.txt,.md",
+                    id="document-input",
+                ),
+                Img(
+                    src="SVG-Loaders-master/svg-loaders/ball-triangle.svg", cls="ui-indicator"
+                ),
+                hx_post="/upload-documents",
+                hx_trigger="change",
+                hx_target="#documents-info",
+            ),
+            Div(id="documents-info"),
+        ]
+    return Div(*components, divider(), id="documents-upload", hx_swap_oob=swap_oob)
 
 
-def questionnaire_input(hx_swap_oob: str = "false"):
-    return Input(
-        type="file",
-        name="questionnaire",
-        accept=".csv",
-        id="questionnaire-input",
-        hx_swap_oob=hx_swap_oob,
-    )
+def questionnaire_upload(current_index: str, swap_oob: str | None = None):
+    components = [H3("Upload Questionnaire CSV")]
+    if not current_index:
+        components.append(
+            P("No index selected. Please select an index to upload questionnaire.")
+        )
+    else:
+        components += [
+            H5(f"Index: {current_index}", id="questionnaire-index") if current_index else None,
+            Form(
+                Input(
+                    type="file",
+                    name="questionnaire",
+                    accept=".csv",
+                    id="questionnaire-input",
+                ),
+                hx_post="/upload-questionnaire",
+                hx_trigger="change",
+                hx_target="#questionnaire-info",
+            ),
+            Div(id="questionnaire-info"),
+        ]
+    return Div(*components, divider(), id="questionnaire-upload", hx_swap_oob=swap_oob)
 
 
 @app.get("/")
 async def home(index: str = ""):
     indexes = get_sorted_indexes()
     current_index = index or indexes[0] if indexes else index
-    container_elements = [
+    container_components = [
         H1("Business Questionnaire Tool"),
         divider(),
         Div(
-            Select(
-                *[
-                    Option(index, value=index, selected=index == current_index)
-                    for index in indexes
-                ],
-                name="index",
-                id="index-select",
-                hx_post="/select-index",
-                hx_target="#index-info",
-                hx_trigger="change, load",
-            )
-            if indexes
-            else None,
-            Input(
-                type="text",
-                name="index",
-                placeholder="Create New Index",
-                hx_post="/select-index",
-                hx_target="#index-info",
-                hx_trigger="keyup[key=='Enter']",
+            Div(
+                Select(
+                    *[
+                        Option(index, value=index, selected=index == current_index)
+                        for index in indexes
+                    ],
+                    name="index",
+                    id="index-select",
+                    hx_post="/select-index",
+                    hx_target="#index-info",
+                    hx_trigger="change, load",
+                    cls="dropdown",
+                )
+                if indexes
+                else None,
+                Input(
+                    type="text",
+                    name="index",
+                    placeholder="Create New Index",
+                    hx_post="/select-index",
+                    hx_target="#index-info",
+                    hx_trigger="keyup[key=='Enter']",
+                ),
+                id="index-controls",
             ),
-            id="index-controls",
+            Div(id="index-info"),
         ),
-        Div(id="index-info"),
         divider(),
-        Div(
-            H3("Upload Documents"),
-            Form(
-                document_input(),
-                hx_post="/upload-documents",
-                hx_trigger="change",
-                hx_target="#documents-info",
-                hx_indicator="#upload-indicator",
-            ),
-            Img(
-                src="SVG-Loaders-master/svg-loaders/ball-triangle.svg",
-                id="upload-indicator",
-                cls="ui-indicator",
-            ),
-        ),
-        Div(id="documents-info"),
-        divider(),
-        Div(
-            H3("Upload your questionnaire CSV file"),
-            Form(
-                questionnaire_input(),
-                hx_post="/upload-questionnaire",
-                hx_trigger="change",
-                hx_target="#questionnaire-info",
-            ),
-        ),
-        Div(id="questionnaire-info"),
+        documents_upload(current_index=current_index),
+        questionnaire_upload(current_index=current_index),
     ]
-
-    return Container(*container_elements)
+    return Main(*container_components, cls="container")
 
 
 @app.get("/index-info")
 def index_info(index: str, index_folder: str | Path = LANCE_DIR):
     index = index.strip().replace(" ", "_")
     existing_tables = get_sorted_indexes(Path(index_folder) / index)
-    div = [H6(f"Selected/Created index: {index}")]
+    div = [H5(f"Index: {index}")]
     if existing_tables:
-        div.append(P(f"{len(existing_tables)} Existing tables:"))
+        div.append(P(f"{len(existing_tables)} Existing Data Sources:"))
         div.append(
             Ul(
                 *[Li(table) for table in existing_tables],
@@ -165,7 +163,7 @@ def index_info(index: str, index_folder: str | Path = LANCE_DIR):
             )
         )
     else:
-        div.append(P("No tables found. Upload documents to create tables."))
+        div.append(P("No data sources found. Upload documents to create data sources."))
     return Div(*div, id="index-info")
 
 
@@ -173,10 +171,8 @@ def index_info(index: str, index_folder: str | Path = LANCE_DIR):
 def select_index(index: str = ""):
     return (
         index_info(index=index),
-        document_input(hx_swap_oob="true"),
-        Div(id="documents-info", hx_swap_oob="true"),
-        questionnaire_input(hx_swap_oob="true"),
-        Div(id="questionnaire-info", hx_swap_oob="true"),
+        documents_upload(current_index=index, swap_oob="outerHTML"),
+        questionnaire_upload(current_index=index, swap_oob="outerHTML"),
     )
 
 
